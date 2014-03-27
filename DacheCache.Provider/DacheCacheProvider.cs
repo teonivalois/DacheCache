38e2c11bcfbb80bca4b3538841af8d5bf31e25ca
teonivalois <teonivalois@gmail.com>
2014-03-27 00:07:53 -0300

    Fixed the CachePolicy and the CacheProvider Also updated the libraties

--- DacheCache.Provider/DacheCacheProvider.cs
+++ DacheCache.Provider/DacheCacheProvider.cs
@@ -2,95 +2,23 @@
 using EFCachingProvider.Caching;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
 using System.Threading.Tasks;
-using System.Web;
 using System.Xml;
 
 namespace DacheCache.Provider {
-    public sealed class DacheCache : ICache {
+    public sealed class DacheCacheProvider : ICache {
 
         private static readonly string _entitySetRelationsKey = "__EntitySetRelationship__";
 
         private static readonly CacheClient _client = new CacheClient();
 
-        public DacheCache() {
+        public DacheCacheProvider() {
             List<CacheEntry> relationships = null;
             if (!_client.TryGet<List<CacheEntry>>(_entitySetRelationsKey, out relationships)) {
                 relationships = new List<CacheEntry>();
                 _client.AddOrUpdate(_entitySetRelationsKey, relationships);
             }
-        }
-
-        public bool ShouldCache(string rawKey, Dictionary<string, string> parameters) {
-            string hash = String.Join("", MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(rawKey)).Select(x => x.ToString("X2")));
-            string type = rawKey.Substring(0, rawKey.IndexOf('|')).Trim();
-            string command = rawKey.Substring(rawKey.IndexOf('|') + 1).Trim();
-            command = command.Replace("\n", string.Empty);
-            command = command.Replace("\t", string.Empty);
-            command = command.Replace("\r", string.Empty);
-
-            string path = null;
-            if (!string.IsNullOrEmpty(ConfigurationManager.AppSettings["DacheCache.ConfigFile"])) {
-                path = ConfigurationManager.AppSettings["DacheCache.ConfigFile"];
-            }
-
-            if (path == null) {
-                return true;
-            } else if (path.StartsWith("~/") && HttpContext.Current != null) {
-                path = HttpContext.Current.Server.MapPath(path);
-            }
-
-            bool isLearning = true;
-            if (!string.IsNullOrEmpty(ConfigurationManager.AppSettings["DacheCache.Learning"])) {
-                Boolean.TryParse(ConfigurationManager.AppSettings["DacheCache.Learning"], out isLearning);
-            }
-
-            XmlDocument document = new XmlDocument();
-            if (File.Exists(path)) {
-                document.Load(path);
-            } else {
-                AddElement(document, "DacheCache", null, document);
-            }
-
-            XmlNodeList nodes = document.SelectNodes("/DacheCache/Entry[@ID='" + hash + "']");
-            foreach (XmlNode item in nodes) {
-                if (item.InnerText.Equals(command, StringComparison.InvariantCultureIgnoreCase)) {
-                    try {
-                        return Convert.ToBoolean(item.Attributes["Cacheable"].Value);
-                    } catch {
-                        return false;
-                    }
-                }
-            }
-
-            if (isLearning) {
-                XmlElement newEntryNode = AddElement(document, "Entry", command, document.LastChild);
-                newEntryNode.Attributes.Append(CreateAttribute(document, "ID", hash));
-                newEntryNode.Attributes.Append(CreateAttribute(document, "Type", type));
-                newEntryNode.Attributes.Append(CreateAttribute(document, "Cacheable", "false"));
-                document.Save(path);
-            }
-            return false;
-        }
-
-        private static XmlElement AddElement(XmlDocument document, string name, string innerText, XmlNode parentNode) {
-            XmlElement element = document.CreateElement(name);
-            element.InnerText = innerText;
-            if (parentNode != null) {
-                parentNode.AppendChild(element);
-            }
-            return element;
-        }
-
-        private XmlAttribute CreateAttribute(XmlDocument document, string name, string value) {
-            XmlAttribute attribute = document.CreateAttribute(name);
-            attribute.Value = value;
-            return attribute;
         }
 
         public bool GetItem(string key, out object value) {
