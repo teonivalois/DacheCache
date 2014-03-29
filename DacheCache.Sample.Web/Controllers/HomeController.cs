5c14f4f1c9d6bc576f575d0c8f418485797c44ec
teonivalois <teonivalois@gmail.com>
2014-03-29 13:29:58 -0300

    Added a sample that only requires changes in Web.Config to enable Caching

---
+++ DacheCache.Sample.Web/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using DacheCache.Sample.Web.Models;
+using DacheCache.Sample.Web.Models.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DacheCache.Sample.Web.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            using (AppDataContext ctx = new AppDataContext()) {
+                List<Person> people = ctx.People.ToList();
+            }
+            return View();
+        }
+
+        public ActionResult About() {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact() {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DacheCache.Sample.Web/Models/Context/AppDataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+
+namespace DacheCache.Sample.Web.Models.Context {
+    public class AppDataContext : DbContext {
+
+        public DbSet<Person> People { get; set; }
+
+        static AppDataContext() {
+            Database.SetInitializer<AppDataContext>(null);
+        }
+
+        public AppDataContext()
+            : base("connection") {
+        }
+
+    }
+}
