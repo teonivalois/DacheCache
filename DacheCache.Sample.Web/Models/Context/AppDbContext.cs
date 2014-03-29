63d96b28a3e0ca38f4a3f2179e30485ad272a786
teonivalois <teonivalois@gmail.com>
2014-03-29 16:12:27 -0300

    Adding the entities

---
+++ DacheCache.Sample.Web/Models/Context/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DacheCache.Sample.Web.Models.Context {
+    public class AppDbContext : DbContext {
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        static AppDbContext() {
+            Database.SetInitializer<AppDbContext>(null);
+        }
+
+        public AppDbContext()
+            : base("connection") {
+        }
+
+    }
+}
---
+++ DacheCache.Sample.Web/Models/Supplier.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DacheCache.Sample.Web.Models {
+    public class Supplier {
+    }
+}
