badfb92c681ce4f4e2f4a2ce3740a24810d12a8d
teonivalois <teonivalois@gmail.com>
2014-03-29 16:14:41 -0300

    Fixing the DbContext Class

--- DacheCache.Sample.Web/Models/Context/AppDbContext.cs
+++ DacheCache.Sample.Web/Models/Context/AppDbContext.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
-namespace DacheCache.Sample.Web.Models.Context
-{
-	public class AppDbContext
-	{
-	}
+namespace DacheCache.Sample.Web.Models.Context {
+    public class AppDbContext :DbContext {
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        static AppDbContext() {
+            Database.SetInitializer<AppDbContext>(null);
+        }
+
+        public AppDbContext()
+            : base("connection") {
+        }
+    }
 }
