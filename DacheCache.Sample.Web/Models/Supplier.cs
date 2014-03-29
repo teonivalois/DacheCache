ec2eaa1d03d824474693892b0b51f7d4ac37b221
teonivalois <teonivalois@gmail.com>
2014-03-29 16:15:18 -0300

    Adding Properties to the Supplier class

--- DacheCache.Sample.Web/Models/Supplier.cs
+++ DacheCache.Sample.Web/Models/Supplier.cs
@@ -1,9 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace DacheCache.Sample.Web.Models {
     public class Supplier {
+
+        [Key, Required]
+        public int SupplierID { get; set; }
+
+        [Required]
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string ContactTitle { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string HomePage { get; set; }
+
     }
 }
